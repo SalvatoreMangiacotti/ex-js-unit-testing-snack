0c93071cabfd6a3c628a322130ff3bd55249cd16
Salvatore <workstudioms@gmail.com>
2025-07-07 21:08:20 +0200

    fix: challenge complete

--- snacks.test.js
+++ snacks.test.js
@@ -3,90 +3,59 @@ const { default: expect } = require('expect');
 
 const { getInitials, createSlug, average, isPalindrome, findPostById } = require('./snacks');
 
-// SNACK 1
+// CHALLENGE
+describe('Manipolazione di Stringhe', () => {
 
-describe("getInitials", () => {
+    // SNACK 1
     test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
         expect(getInitials("Buzz Lightyear")).toBe("BL");
     })
-})
 
-
-// SNACK 2 - 4 - 6
-
-describe("createSlug", () => {
-    // SNACK 2
-    test("La funzione createSlug restituisce una stringa in lowercase.", () => {
-        expect(createSlug("JAVASCRIPT")).toBe("javascript");
+    // SNACK 5
+    test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
+        expect(isPalindrome("Anna ")).toBeTruthy();
     })
 
-    // SNACK 4
-    test("La funzione createSlug sostituisce gli spazi con -.", () => {
-        expect(createSlug("Questo è un test")).toBe("questo-è-un-test");
-    })
-
-    // SNACK 6
-    test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido.", () => {
-        expect(() => createSlug("")).toThrow("Titolo non valido");
-        expect(() => createSlug(null)).toThrow("Titolo non valido");
-    })
 })
 
 
 // SNACK 3
-
 const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-describe("average", () => {
-    test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
-        expect(average(numbersArray)).toBe(5.5);
-    })
-})
-
-
-// SNACK 5
-
-describe("isPalindrome", () => {
-    test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
-        expect(isPalindrome("Anna ")).toBeTruthy();
-    })
-})
-
 
 // SNACK 7
 
-let posts;
+const posts = [
+    {
+        id: 1,
+        title: "Le auto sportive più veloci del mondo",
+        slug: "le-auto-sportive-piu-veloci-del-mondo"
+    },
+    {
+        id: 2,
+        title: "Come scegliere un'auto sportiva",
+        slug: "come-scegliere-un-auto-sportiva"
+    },
+    {
+        id: 3,
+        title: "Le innovazioni tecnologiche nelle auto sportive",
+        slug: "le-innovazioni-tecnologiche-nelle-auto-sportive"
+    },
+    {
+        id: 4,
+        title: "I migliori motori per auto sportive",
+        slug: "i-migliori-motori-per-auto-sportive"
+    }
+];
 
-beforeEach(() => {
-    posts = [
-        {
-            id: 1,
-            title: "Le auto sportive più veloci del mondo",
-            slug: "le-auto-sportive-piu-veloci-del-mondo"
-        },
-        {
-            id: 2,
-            title: "Come scegliere un'auto sportiva",
-            slug: "come-scegliere-un-auto-sportiva"
-        },
-        {
-            id: 3,
-            title: "Le innovazioni tecnologiche nelle auto sportive",
-            slug: "le-innovazioni-tecnologiche-nelle-auto-sportive"
-        },
-        {
-            id: 4,
-            title: "I migliori motori per auto sportive",
-            slug: "i-migliori-motori-per-auto-sportive"
-        }
-    ];
-})
 
-afterEach(() => {
-    posts = [];
-})
+describe('Operazioni su Array', () => {
 
-describe("findPostById", () => {
+    // SNACK 3
+    test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+        expect(average(numbersArray)).toBe(5.5);
+    })
+
+    // SNACK 7
     test("restituisce il post corretto dato l'array di post e l'id", () => {
         const postToCheck = posts[2];
         expect(findPostById(posts, 3)).toEqual(postToCheck)
@@ -102,4 +71,28 @@ describe("findPostById", () => {
     //         expect(post).toHaveProperty("slug");
     //     });
     // })
+
 })
+
+describe('Generazione di Slug', () => {
+
+    // SNACK 2
+    test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+        expect(createSlug("JAVASCRIPT")).toBe("javascript");
+    })
+
+    // SNACK 4
+    test("La funzione createSlug sostituisce gli spazi con -.", () => {
+        expect(createSlug("Questo è un test")).toBe("questo-è-un-test");
+    })
+
+    // SNACK 6
+    test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido.", () => {
+        expect(() => createSlug("")).toThrow("Titolo non valido");
+        expect(() => createSlug(null)).toThrow("Titolo non valido");
+    })
+
+})
+
+
+
